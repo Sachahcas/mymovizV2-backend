24999ca261ffb3821aebd4746d1654fc143bd226
Your name <Your email>
2023-07-05 15:13:51 +0200

    backend mymovizV2

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const apiKey = process.env.API_KEY 
+
+router.get('/movies', (req,res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+    .then(response=>response.json())
+    .then(data => {
+        res.json({movies : data.results})
+    })
+})
+
+module.exports = router;
